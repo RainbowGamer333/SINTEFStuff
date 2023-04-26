af883c153980baea5ac2af6235475e917908b69b
Romain Patureau <romain.patureau@outlook.com>
2023-04-26 10:56:28 +0200

    convert scipt to class

--- neoflix/src/openAI/chatbot.py
+++ neoflix/src/openAI/chatbot.py
@@ -6,59 +6,75 @@ from datetime import datetime
 
 # todo: implement the API into the rest of the code
 
-def fixed_invoke(messages):
-    return openai.ChatCompletion.create(
-        engine="gpt-35",
-        messages=messages,
-        temperature=0,
-        max_tokens=200,
-        top_p=0.95,
-        frequency_penalty=0,
-        presence_penalty=0,
-        stop=None
-    )
+class ChatBot:
+    def __init__(self, promptPath):
+        remoteapi.loadCredential()
+        self.usage = {"completion_tokens": 0, "prompt_tokens": 0, "total_tokens": 0}
+        with open(promptPath, "r") as prompt:
+            self.messages = [{'role': 'system', 'content': prompt.read()}]
+
+    def get_ai_response(self):
+        return openai.ChatCompletion.create(
+            engine="gpt-35",
+            messages=self.messages,
+            temperature=0,
+            max_tokens=200,
+            top_p=0.95,
+            frequency_penalty=0,
+            presence_penalty=0,
+            stop=None
+        )
+
+    def log_conversation(self):
+        f = open('../log/' + datetime.now().strftime("%Y%m%d-%H.%M.%S") + '.txt', 'w')
+        for item in self.messages:
+            f.write('[' + item['role'] + ']: ')
+            f.write(item['content'] + '\n')
+        f.write(str(self.usage))
+        f.close()
 
 
-messages = []
-usage = {"completion_tokens": 0, "prompt_tokens": 0, "total_tokens": 0}
+    def ask(self):
+        question = input(colored('Continue, or type "END"\n> '))
+        if question == 'END':
+            self.log_conversation()
+            return False
 
+        self.messages.append({'role': 'user', 'content': question})
+        response = self.get_ai_response()
+        reply = response['choices'][0]['message']
+        self.messages.append(reply)
+        print(reply['content'])
 
-def init():
-    hint = input(colored("What is the role of API?\n> "))
-    messages.append({'role': 'system', 'content': hint})
+        oneusage = response['usage']
+        for i in self.usage:
+            self.usage[i] = self.usage[i] + oneusage[i]
 
-
-def log_conversation():
-    f = open('../log/' + datetime.now().strftime("%Y%m%d-%H.%M.%S") + '.txt', 'w')
-    for item in messages:
-        f.write('[' + item['role'] + ']: ')
-        f.write(item['content'] + '\n')
-    f.write(str(usage))
-    f.close()
-
-
-def ask():
-    question = input(colored('Continue, or type "END"\n> '))
-    if question == 'END':
-        log_conversation()
         return True
-
-    messages.append({'role': 'user', 'content': question})
-    response = fixed_invoke(messages)
-    reply = response['choices'][0]['message']
-    messages.append(reply)
-    print(reply['content'])
-
-    oneusage = response['usage']
-    for i in usage:
-        usage[i] = usage[i] + oneusage[i]
-
-    return False
 
 
 if __name__ == "__main__":
-    remoteapi.loadCredential()
-    init()
-    while True:
-        if ask():
-            break
+    chat = ChatBot("../prompt/prompt.txt")
+    while chat.ask():
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
