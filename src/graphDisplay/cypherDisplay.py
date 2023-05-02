6842d03f064b140b19c661108bb6c97f63d706ce
Romain Patureau <romain.patureau@outlook.com>
2023-05-02 10:36:50 +0200

    cleaned authentication

--- src/graphDisplay/cypherDisplay.py
+++ src/graphDisplay/cypherDisplay.py
@@ -13,11 +13,11 @@ class CypherDisplay:
     A class to handle the execution of cypher queries and to display the results.
     """
 
-    def __init__(self, sch, h, p, user=None, password=None, query=None):
+    def __init__(self, sch, h, p, auth=(None, None), query=None):
         """
         Initialise the class.
         """
-        self.graph = Graph(scheme=sch, host=h, port=p, auth=(user, password))
+        self.graph = Graph(scheme=sch, host=h, port=p, auth=(auth[0], auth[1]))
         self.data = self.execute_query(query) if query is not None else None
         self.__nodeDisplay = {'Person': {'display': 'name', 'colour': 'green'},
                               'Movie': {'display': 'title', 'colour': 'red'},
@@ -58,21 +58,22 @@ class CypherDisplay:
         :return: networkx MultiDiGraph containing the data returned from the query
         """
 
+        assert self.data is not None, 'No data has been set. Execute a query first.'
         g = nx.MultiDiGraph()
         for record in self.data:
             for value in record.values():
 
                 # if node
                 if isinstance(value, py2neo.data.Node):
-                    display, colour = self.get_node_display_and_colour(value)
+                    display, colour = self.get_node_display(value)
                     g.add_node(display, color=colour)
 
 
                 # if relationship
                 elif isinstance(value, py2neo.data.Relationship):
                     rel = type(value).__name__
-                    startDisplay, _ = self.get_node_display_and_colour(value.start_node)
-                    endDisplay, _ = self.get_node_display_and_colour(value.end_node)
+                    startDisplay, _ = self.get_node_display(value.start_node)
+                    endDisplay, _ = self.get_node_display(value.end_node)
 
                     if not g.has_edge(startDisplay, endDisplay):
                         g.add_edge(startDisplay, endDisplay, label=rel, arrows="to", color="grey")
@@ -84,7 +85,7 @@ class CypherDisplay:
                             g.add_edge(startDisplay, endDisplay, label=rel, arrows="to", color="grey")
         return g
 
-    def get_node_display_and_colour(self, node):
+    def get_node_display(self, node):
         """
         Get the display and colour of a node.
         If the node has the correct display attribute then that will be used, otherwise the node's identity will be used.
@@ -97,23 +98,22 @@ class CypherDisplay:
 
         return display, colour
 
-    def create_html_graph(self, fname=None):
+    def create_html_graph(self, filename=None):
         """
         Display the result of a query as a network graph. You can view the graph as a html file.
-        :param fname: the name of the file to be created. Will be automatically appended with '.html' if not already.
+        :param filename: the name of the file to be created. Will be automatically appended with '.html' if not already.
         If None then the function will use the current self.filename
         :return: None
         """
-        assert fname is not None or self.filename is not None, 'No filename has been set'
+        assert filename is not None or self.filename is not None, 'No filename has been set'
 
-        if fname is not None:
-            self.set_filename(fname)
+        if filename is not None:
+            self.set_filename(filename)
 
         g = self.get_graph_from_data()
 
         nt = Network(notebook=True, directed=True, height="1400px", cdn_resources="remote")
         nt.from_nx(g)
-        # nt.set_edge_smooth('dynamic')
 
         current_dir = os.path.dirname(os.path.abspath(__file__))
         graph_path = os.path.join(os.path.dirname(current_dir), 'graphs', self.filename)
@@ -147,7 +147,7 @@ class CypherDisplay:
         """
         Add a node display to the nodeDisplay dictionary. If the label already exists, then it will be replaced.
         :param label: the name of the node
-        :param display: the attribute of the node to be displayed
+        :param display: the attribute of the node to be displayed. If a node does not have this attribute then its identity will be used.
         :param colour: the colour of the node
         :return: None
         """
@@ -155,7 +155,7 @@ class CypherDisplay:
 
 
 if __name__ == '__main__':
-    cypher = CypherDisplay('https', 'demo.neo4jlabs.com', 7473, 'recommendations', 'recommendations')
-    cypher.execute_query("match (n)-[r]->(m) return n,r,m limit 10")
+    cypher = CypherDisplay('https', 'demo.neo4jlabs.com', 7473, auth=('recommendations', 'recommendations'))
+    cypher.execute_query("match (n) return n limit 10")
     cypher.create_html_graph('example')
     cypher.open_graph('example')
