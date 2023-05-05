ae260ac622456bc7ea43cce1c38730dc8ed918c1
Romain Patureau <romain.patureau@outlook.com>
2023-05-05 16:03:23 +0200

    completed procedure. Improvement needed

--- src/main.py
+++ src/main.py
@@ -1,40 +1,45 @@
 from cypherInterpretation.graphDisplay import GraphDisplay
 from openAI.chatbot import ChatBot
 
-def start_conversation(graph, bot):
+def start_conversation(connection):
+    # queryBot is used to get the query from the user's question
+    queryBot = ChatBot(prompt="questionToQuery.txt", history=True, log=False)
+    # finalBot is used to get the response from the data retrieved from the query
+    finalBot = ChatBot(prompt="dataToNatural.txt", history=True)
+
     while True:
         # ask question
         question = input('Type in your question, or type "QUIT"\n> ')
         if question.lower() == 'quit':
-            bot.log_conversation()
+            finalBot.log_conversation()
             return
-        bot.add_message('user', question)
+        queryBot.add_message('user', question)
 
-        # get chatbot reply
-        query = bot.reply()
-        print(query)
+        # get queryBot reply
+        query = queryBot.reply(add_message=True)
+
         # execute query and return data
-        graph.execute_query(query)
-        g = graph.create_graph('graph.html', result=True)
+        connection.execute_query(query)
+        graph = connection.create_graph('graph.html', result=True)
 
-        # get data and return response
+        # get finalBot reply
+        finalBot.add_message('system', str(graph.nodes))
+        finalBot.add_message('user', question)
+        response = finalBot.reply(add_message=True)
+        display_response(response)
 
-        newBot = ChatBot(prompt="dataToNatural.txt", history=True, log=False)
-        newBot.add_message('system', str(g.nodes))
-        newBot.add_message('user', question)
-        response = newBot.reply()
-        bot.add_message('assistant', response)
-        print(response)
-
+        # display graph
         showGraph = ''
         while showGraph not in ['y', 'n']:
             showGraph = input("Would you like to display the graph? (y/n)\n> ")
             if showGraph == 'y':
-                graph.open_graph('graph.html')
+                connection.open_graph('graph.html')
 
+
+def display_response(answer):
+    print(answer)
 
 
 if __name__ == "__main__":
-    connection = GraphDisplay('https', 'demo.neo4jlabs.com', 7473, auth=('recommendations', 'recommendations'))
-    chatbot = ChatBot(prompt="questionToQuery.txt", history=True)
-    start_conversation(connection, chatbot)
+    graphDisplay = GraphDisplay('https', 'demo.neo4jlabs.com', 7473, auth=('recommendations', 'recommendations'))
+    start_conversation(graphDisplay)
