fb132f4857222fb51e292270d093a49ec88017f4
Romain Patureau <romain.patureau@outlook.com>
2023-05-09 14:54:02 +0200

    test commit

--- src/main.py
+++ src/main.py
@@ -4,3 +4,4 @@ from conversation import *
 if __name__ == "__main__":
     graphDisplay = GraphDisplay('https', 'demo.neo4jlabs.com', 7473, auth=('recommendations', 'recommendations'))
     start_conversation(graphDisplay)
+
