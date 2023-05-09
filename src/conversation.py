d9d08d05f7cde7b29a25dee7440b24fbaf998623
Romain Patureau <romain.patureau@outlook.com>
2023-05-09 14:22:19 +0200

    resolving git issues

--- src/conversation.py
+++ src/conversation.py
@@ -1,4 +1,4 @@
-from openAI.chatbot import ChatBot
+from openAI.chatbot import *
 
 def start_conversation(connection, graphName='graph.html'):
     # queryBot is used to get the query from the user's question
@@ -37,3 +37,36 @@ def start_conversation(connection, graphName='graph.html'):
             showGraph = input("Would you like to display the graph? (y/n)\n> ")
             if showGraph == 'y':
                 connection.open_graph(graphName)
+
+
+def continue_conversation(connection, messages):
+
+    new_messages = format_all_messages(messages)
+    # queryBot is used to get the query from the user's question
+    queryBot = ChatBot(prompt="questionToQuery.txt", temperature=0.6, max_tokens=200, presence_penalty=-1, history=True, log=False)
+    queryBot.messages = new_messages
+    # finalBot is used to get the response from the data retrieved from the query
+    finalBot = ChatBot(prompt="dataToNatural.txt", temperature=0.05, max_tokens=1000, presence_penalty=-2, history=True)
+    finalBot.messages = new_messages
+
+    question = new_messages[-1]['content']
+    query = queryBot.reply(add_message=True)
+
+    connection.execute_query(query)
+    graph = connection.create_graph('graph.html', result=True)
+    nodes = str(graph.nodes)
+    queryBot.add_message('system', nodes)
+
+    finalBot.add_message('user', question)
+    finalBot.add_message('system', nodes)
+    response = finalBot.reply(add_message=True)
+
+    return response
+
+
+def format_all_messages(messages):
+    new_messages = []
+    for message in messages:
+        new_messages.append(format_message(message['role'], message['content']))
+    print(new_messages)
+    return new_messages
--- src/server.py
+++ src/server.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
+from cypherInterpretation.graphDisplay import GraphDisplay
+from conversation import *
 
 app = Flask(__name__, template_folder='templates')
 
+messages = [{'role': 'assistant', 'time': datetime.now().strftime("%H:%M"), 'content': 'Welcome to the Neo4j Chatbot!'}]
 
-@app.route("/")
+@app.route("/", methods=('GET', 'POST'))
 def load_index():
-    return render_template('index.html')
+    graphDisplay = GraphDisplay('https', 'demo.neo4jlabs.com', 7473, auth=('recommendations', 'recommendations'))
+    if request.method == 'POST':
+        question = request.form['user-message']
+        reply(graphDisplay, question)
+
+    return render_template('index.html', messages=messages, connection=graphDisplay)
+
+def reply(connection, question):
+    messages.append({'role': 'user', 'time': datetime.now().strftime("%H:%M"), 'content': question})
+    response = continue_conversation(connection, messages=messages)
+    messages.append({'role': 'assistant', 'time': datetime.now().strftime("%H:%M"), 'content': response})
+
+
+
 
 
 if __name__ == "__main__":
