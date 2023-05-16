36fe17ba5ad1ca871af706f89ab02a094053eefb
Romain Patureau <romain.patureau@outlook.com>
2023-05-16 15:41:02 +0200

    implementing graph display to template

--- flask/app.py
+++ flask/app.py
@@ -2,8 +2,8 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+from flask import Flask, render_template, request, url_for
 from datetime import datetime
-from flask import Flask, render_template, request
 from py2neo.errors import ClientError
 from openai.error import APIError
 
@@ -14,18 +14,24 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route("/", methods=['GET', 'POST'])
 def load_index():
+    print(url_for('static', filename='style.css'))
     if request.method == 'POST':
+        # if button clear is pressed, reset messages
         try:
             request.form['clear']
             reset_messages()
         except Exception:
             question = request.form['user-message']
+            print("question : ", question)
             try:
                 reply(question)
-                return render_template('index.html', messages=messages, connection=graphDisplay, error=False)
+                
+                graph_name='graph.html'
+                return render_template('index.html', connection=graphDisplay, messages=messages, graph_name=graph_name, error=False)
+            # if user input cannot be interpreted, display error message
             except (ClientError, APIError):
-                add_message('assistant', 'An error has occurred. Please try again...')
-                return render_template('index.html', messages=messages, connection=graphDisplay, error=True)
+                add_message('assistant', 'An error has occurred. Please clear conversation...')
+                return render_template('index.html', connection=graphDisplay, messages=messages, error=True)
 
     return render_template('index.html', messages=messages, connection=graphDisplay, error=False)
 
@@ -39,7 +45,7 @@ def reply(question):
 def reset_messages():
     global messages
     messages = []
-    add_message('assistant', 'Welcome to the Neo4j Chatbot!')
+    add_message('assistant', 'Welcome to the Neo4j Chatbot')
 
 
 def add_message(role, message):
